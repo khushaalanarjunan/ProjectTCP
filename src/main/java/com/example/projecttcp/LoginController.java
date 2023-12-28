eabd3947d77d4ee355e802c57cb72ae0eb3960e1
Chuo <ignatius.chuo@seamlessx.com>
2023-12-29 00:38:55 +0800

    feat: separate LoginController

---
+++ src/main/java/com/example/projecttcp/LoginController.java
@@ -0,0 +1,39 @@
+package com.example.projecttcp;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+public class LoginController {
+    @FXML
+    private Button btn_login;
+
+    @FXML
+    private TextField tf_username;
+
+    @FXML
+    private void login(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("chat-list.fxml"));
+        try {
+            Parent root = fxmlLoader.load();
+            var chatController = (ChatController) fxmlLoader.getController();
+            chatController.setUsername(tf_username.getText());
+
+            Stage stage = (Stage) ((javafx.scene.Node) event.getSource()).getScene().getWindow();
+            stage.setOnHidden(e->{
+                chatController.close();
+            });
+            stage.getScene().setRoot(root);
+        } catch (Exception e) {
+            System.out.println(e.toString());
+            e.printStackTrace();
+        }
+
+    }
+}
